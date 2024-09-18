57f3bcc5e51915c4b6aef3e9b23a37f4ac8ea774
FrostyJW <palavandishvilidima1@gmail.com>
2024-09-18 17:18:21 +0400

    Added Configuration for SQLLite.

---
+++ ECommMarket.Persistence/Data/MarketDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommMarket.Persistence.Data
+{
+    public class MarketDbContext : DbContext
+    {
+        public MarketDbContext(DbContextOptions<MarketDbContext> options) : base(options)
+        {
+        }
+    }
+}
