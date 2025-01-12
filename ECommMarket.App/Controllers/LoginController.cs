23cb48a6b91d8432a3a6272390fdf46fa43b7cec
APalavandishvili <aleksandrepalavandishvili99@gmail.com>
2025-01-12 23:25:30 +0400

    add login

---
+++ ECommMarket.App/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using ECommMarket.App.Models;
+using EcommMarket.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommMarket.App.Controllers;
+
+public class LoginController : Controller
+{
+
+    public async Task<IActionResult> Index(LoginViewModel request)
+    {
+        return View();
+    }
+}
